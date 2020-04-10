87bb4d304e11479c0565609d27e8136ca75b07d9
Denis Averin <denis.averin@aspose.com>
2020-04-10 20:13:03 +0700

    New `swagger-codegen-cli.jar` v2.4.13

--- codegen/generate-net.bat
+++ codegen/generate-net.bat
@@ -1,14 +1,37 @@
 cls
 set tempDir=.generated
-set targetDir=..\..\aspose-barcode-cloud-dotnet\src
+set targetDir=..\..\aspose-barcode-cloud-dotnet
+
+rem set specSource=http://localhost:47972/v3.0/barcode/swagger/spec
+rem set specSource=https://api.aspose.cloud/v3.0/barcode/swagger/spec
+set specSource=..\spec\aspose-barcode-cloud.json
 
 if exist %tempDir% del /s /q %tempDir% || goto :error
-java -jar Tools\swagger-codegen-cli.jar generate -i ..\spec\aspose-barcode-cloud.json -l csharp -t Templates\csharp -o %tempDir% -c config.json || goto :error
+
+
+REM Generate Operations and Models for Debug purposes
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugOperations.cs.json || goto :error
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugModels.cs.json || goto :error
+java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json || goto :error
 
 Tools\SplitCSharpCodeFile.exe %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\BarCodeApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
+Tools\SplitCSharpCodeFile.exe %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\FileApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
+Tools\SplitCSharpCodeFile.exe %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\FolderApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
+Tools\SplitCSharpCodeFile.exe %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\StorageApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
 
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model" "%targetDir%\Model\" /e /i /y || goto :error
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api" "%targetDir%\Api\" /e /i /y || goto :error
 
-del /s /q %tempDir% || goto :error
-rmdir /s /q  %tempDir% || goto :error
+del /s /q "%targetDir%\src\Model\" > NUL || goto :error
+xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model" "%targetDir%\src\Model\" /e /i /y > NUL || goto :error
+
+del /s /q "%targetDir%\src\Api\*Api.cs" > NUL || goto :error
+xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\*Api.cs" "%targetDir%\src\Api\" /e /i /y > NUL || goto :error
+
+del /s /q "%targetDir%\docs" > NUL || goto :error
+xcopy "%tempDir%\docs" "%targetDir%\docs" /e /i /y > NUL || goto :error
+
+xcopy "%tempDir%\README.md" "%targetDir%\" /y > NUL || goto :error
+xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Aspose.BarCode.Cloud.Sdk.csproj" "%targetDir%\src\Aspose.BarCode.Cloud.Sdk.csproj" /y > NUL || goto :error
+
+
+del /s /q %tempDir% > NUL || goto :error
+rmdir /s /q %tempDir% > NUL || goto :error
