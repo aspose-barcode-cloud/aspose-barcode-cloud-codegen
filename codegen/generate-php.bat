ed948b7d4b911cd89511ca1c6f48463ca48d2b9a
Denis Averin <denis.averin@aspose.com>
2020-05-08 21:11:34 +0700

    WIP: Fixed generator code

--- codegen/generate-php.bat
+++ codegen/generate-php.bat
@@ -9,8 +9,12 @@ Tools\SplitPhpCodeFile.exe %tempDir%\SwaggerClient-php\lib\Api\BarCodeApi.php %t
 
 xcopy %tempDir%\SwaggerClient-php\lib\Api %targetDir%\Aspose\BarCode /i /y || goto :error
 xcopy %tempDir%\SwaggerClient-php\lib %targetDir%\Aspose\BarCode /i /y || goto :error
+
+del /s /q "%targetDir%\Aspose\BarCode\Model\" > NUL || goto :error
 xcopy %tempDir%\SwaggerClient-php\lib\Model %targetDir%\Aspose\BarCode\Model /i /y || goto :error
+
+del /s /q "%targetDir%\Aspose\BarCode\Requests\" > NUL || goto :error
 xcopy %tempDir%\SwaggerClient-php\lib\Requests %targetDir%\Aspose\BarCode\Requests /i /y || goto :error
 
-del /s /q %tempDir% || goto :error
-rmdir /s /q  %tempDir% || goto :error
+del /s /q %tempDir% > NUL || goto :error
+rmdir /s /q  %tempDir% > NUL || goto :error
