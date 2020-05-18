5e1a6b2a12ccd09d63fc603c8e1c81d8b9c262f5
Denis Averin <denis.averin@aspose.com>
2020-05-18 20:42:18 +0700

    Docs added

--- codegen/generate-php.bat
+++ codegen/generate-php.bat
@@ -1,20 +1,31 @@
 cls
 set tempDir=.generated
-set targetDir=..\..\aspose-barcode-cloud-php\src
+set targetDir=..\..\aspose-barcode-cloud-php
 
-if exist %tempDir% del /s /q %tempDir% || goto :error
-java -jar Tools\swagger-codegen-cli.jar generate -i ..\spec\aspose-barcode-cloud.json -l php -t Templates\php -o %tempDir% -c config.json || goto :error
+rem set specSource=http://localhost:47972/v3.0/barcode/swagger/spec
+rem set specSource=https://api-qa.aspose.cloud/v3.0/barcode/swagger/spec
+rem set specSource=https://api.aspose.cloud/v3.0/barcode/swagger/spec
+set specSource=..\spec\aspose-barcode-cloud.json
 
-Tools\SplitPhpCodeFile.exe %tempDir%\SwaggerClient-php\lib\Api\BarCodeApi.php %tempDir%\SwaggerClient-php\lib\Requests\ || goto :error
+if exist %tempDir% del /s /q %tempDir% > NUL || goto :error
+java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l php -t Templates\php -o %tempDir% -c config.json || goto :error
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l php -t Templates\php -o %tempDir% -c config.json > debugOperations.php.json || goto :error
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l php -t Templates\php -o %tempDir% -c config.json > debugModels.php.json || goto :error
 
-xcopy %tempDir%\SwaggerClient-php\lib\Api %targetDir%\Aspose\BarCode /i /y || goto :error
-xcopy %tempDir%\SwaggerClient-php\lib %targetDir%\Aspose\BarCode /i /y || goto :error
+Tools\SplitPhpCodeFile.exe %tempDir%\SwaggerClient-php\lib\Api\BarcodeApi.php %tempDir%\SwaggerClient-php\lib\Requests\ || goto :error
 
-del /s /q "%targetDir%\Aspose\BarCode\Model\" > NUL || goto :error
-xcopy %tempDir%\SwaggerClient-php\lib\Model %targetDir%\Aspose\BarCode\Model /i /y || goto :error
+del /s /q "%targetDir%\src\Aspose\Barcode\*Api.php" > NUL || goto :error
+xcopy %tempDir%\SwaggerClient-php\lib\Api %targetDir%\src\Aspose\Barcode /i /y || goto :error
+xcopy %tempDir%\SwaggerClient-php\lib %targetDir%\src\Aspose\Barcode /i /y || goto :error
 
-del /s /q "%targetDir%\Aspose\BarCode\Requests\" > NUL || goto :error
-xcopy %tempDir%\SwaggerClient-php\lib\Requests %targetDir%\Aspose\BarCode\Requests /i /y || goto :error
+del /s /q "%targetDir%\src\Aspose\Barcode\Model\" > NUL || goto :error
+xcopy %tempDir%\SwaggerClient-php\lib\Model %targetDir%\src\Aspose\Barcode\Model /i /y || goto :error
+
+del /s /q "%targetDir%\src\Aspose\Barcode\Requests\" > NUL || goto :error
+xcopy %tempDir%\SwaggerClient-php\lib\Requests %targetDir%\src\Aspose\Barcode\Requests /i /y || goto :error
+
+del /s /q "%targetDir%\docs\" > NUL || goto :error
+xcopy /s /i /y %tempDir%\SwaggerClient-php\docs %targetDir%\docs || goto :error
 
 del /s /q %tempDir% > NUL || goto :error
 rmdir /s /q  %tempDir% > NUL || goto :error
