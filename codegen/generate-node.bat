d32a4f201ff97a132735ddd975225e952c7f5f4b
Denis Averin <denis.averin@aspose.com>
2020-05-21 19:09:53 +0700

    Generate package.json

--- codegen/generate-node.bat
+++ codegen/generate-node.bat
@@ -6,14 +6,16 @@ rem set specSource=https://api.aspose.cloud/v3.0/barcode/swagger/spec
 set specSource=..\spec\aspose-barcode-cloud.json
 
 set tempDir=.generated
-set targetDir=..\..\aspose-barcode-cloud-node\src
+set targetDir=..\..\aspose-barcode-cloud-node
 
 if exist %tempDir% del /s /q %tempDir% || goto :error
 java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json || goto :error
+rem java -jar Tools\swagger-codegen-cli.jar config-help -l typescript-node
 rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json > debugModels.ts.json || goto :error
 rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json > debugOperations.ts.json || goto :error
 
-xcopy "%tempDir%\api.ts" "%targetDir%\api.ts" /e /i /y || goto :error
+xcopy "%tempDir%\api.ts" "%targetDir%\src\api.ts" /y || goto :error
+xcopy "%tempDir%\package.json" "%targetDir%\package.json" /y || goto :error
 
-del /s /q %tempDir% > NUL || goto :error
-rmdir /s /q  %tempDir% > NUL || goto :error
+rem del /s /q %tempDir% > NUL || goto :error
+rem rmdir /s /q  %tempDir% > NUL || goto :error
