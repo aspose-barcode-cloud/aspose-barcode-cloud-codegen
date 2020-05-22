4449fbdd319077ee1b10ad5163759ad13cd64139
Denis Averin <denis.averin@aspose.com>
2020-05-22 12:35:44 +0700

    Readme template added

--- codegen/generate-node.bat
+++ codegen/generate-node.bat
@@ -10,12 +10,15 @@ set targetDir=..\..\aspose-barcode-cloud-node
 
 if exist %tempDir% del /s /q %tempDir% || goto :error
 java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json || goto :error
+REM Use typescript-jquery because typescript-node does not generate README.md
+java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-jquery -t Templates\nodejs -o %tempDir%\jq -c config.json || goto :error
 rem java -jar Tools\swagger-codegen-cli.jar config-help -l typescript-node
 rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json > debugModels.ts.json || goto :error
 rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l typescript-node -t Templates\nodejs -o %tempDir% -c config.json > debugOperations.ts.json || goto :error
 
 xcopy "%tempDir%\api.ts" "%targetDir%\src\api.ts" /y || goto :error
 xcopy "%tempDir%\package.json" "%targetDir%\package.json" /y || goto :error
+xcopy "%tempDir%\jq\README.md" "%targetDir%\README.md" /y || goto :error
 
-rem del /s /q %tempDir% > NUL || goto :error
-rem rmdir /s /q  %tempDir% > NUL || goto :error
+del /s /q %tempDir% > NUL || goto :error
+rmdir /s /q  %tempDir% > NUL || goto :error
