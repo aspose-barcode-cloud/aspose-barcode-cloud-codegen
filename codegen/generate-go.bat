394e9376b9d9145e396ada73210ac887a32ba7af
Denis Averin <denis.averin@aspose.com>
2020-06-11 19:27:01 +0700

    Package renamed from aspose_barcode_cloud to barcode

--- codegen/generate-go.bat
+++ codegen/generate-go.bat
@@ -11,17 +11,22 @@ set targetDir=..\..\aspose-barcode-cloud-go
 if exist %tempDir% del /s /q %tempDir% || goto :error
 
 rem java -jar Tools\swagger-codegen-cli.jar config-help -l go
-java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-python-go.json || goto :error
-rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-python-go.json > debugModels.go.json || goto :error
-rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-python-go.json > debugOperations.go.json || goto :error
+java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-go.json || goto :error
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-go.json > debugModels.go.json || goto :error
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l go -t Templates\go -o %tempDir% -c config-go.json > debugOperations.go.json || goto :error
 
-del /s /q "%targetDir%\aspose_barcode_cloud\" > NUL || goto :error
-copy "%tempDir%\*.go" "%targetDir%\aspose_barcode_cloud\" /y > NUL || goto :error
+del /s /q "%targetDir%\barcode" > NUL || goto :error
+rmdir /s /q  "%targetDir%\barcode" > NUL || goto :error
+mkdir "%targetDir%\barcode\models" > NUL || goto :error
+move /y "%tempDir%\model_*.go" "%targetDir%\barcode\models\" > NUL || goto :error
+mkdir "%targetDir%\barcode\jwt" > NUL || goto :error
+copy /y "%tempDir%\git_push.sh" "%targetDir%\barcode\jwt\jwt.go" > NUL || goto :error
+copy "%tempDir%\*.go" "%targetDir%\barcode\" /y > NUL || goto :error
 
 del /s /q "%targetDir%\docs\" > NUL || goto :error
 xcopy "%tempDir%\docs\*" "%targetDir%\docs\" /e /i /y > NUL || goto :error
 
 copy "%tempDir%\README.md" "%targetDir%\README.md" /y > NUL || goto :error
 
-rem del /s /q %tempDir% > NUL || goto :error
-rem rmdir /s /q  %tempDir% > NUL || goto :error
+del /s /q %tempDir% > NUL || goto :error
+rmdir /s /q  %tempDir% > NUL || goto :error
