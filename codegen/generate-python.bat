ab1f5b42f047bd35ead98959aee00f4772fa2952
Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
2020-06-22 12:15:31 +0700

    Go SDK added (#9)

--- codegen/generate-python.bat
+++ codegen/generate-python.bat
@@ -11,9 +11,9 @@ set targetDir=..\..\aspose-barcode-cloud-python
 if exist %tempDir% del /s /q %tempDir% || goto :error
 
 rem java -jar Tools\swagger-codegen-cli.jar config-help -l python
-java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python.json || goto :error
-rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python.json > debugModels.py.json || goto :error
-rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python.json > debugOperations.py.json || goto :error
+java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python-go.json || goto :error
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python-go.json > debugModels.py.json || goto :error
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l python -t Templates\python -o %tempDir% -c config-python-go.json > debugOperations.py.json || goto :error
 
 del /s /q "%targetDir%\aspose_barcode_cloud\" > NUL || goto :error
 xcopy "%tempDir%\aspose_barcode_cloud\*" "%targetDir%\aspose_barcode_cloud\" /e /i /y > NUL || goto :error
