a23cc9506f90b05339a5730e618782c0e9bc4f11
Denis Averin <denis.averin@aspose.com>
2020-10-29 14:56:10 +0700

    All SDKs updated to 20.10

--- scripts/make-all.cmd
+++ scripts/make-all.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+pushd codegen
 for %%f in (generate-*.bat) do (
     %%f || echo %%f && goto :error
 )
+popd
---
+++ scripts/update_swagger_spec.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+curl https://api.aspose.cloud/v3.0/barcode/swagger/spec > spec/aspose-barcode-cloud.json
