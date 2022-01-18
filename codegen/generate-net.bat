51b273c58e5f5793d30d59066fd171027f18e448
Denis Averin <denis.averin@aspose.com>
2022-01-18 21:17:00 +0700

    Net SDK update

--- codegen/generate-net.bat
+++ codegen/generate-net.bat
@@ -1,18 +1,18 @@
 cls
 
-set tempDir=.generated
+set tempDir=.generated\dotnet
 set targetDir=..\submodules\dotnet
 
 rem set specSource=http://localhost:47972/v3.0/barcode/swagger/spec
 rem set specSource=https://api.aspose.cloud/v3.0/barcode/swagger/spec
 set specSource=..\spec\aspose-barcode-cloud.json
 
-if exist %tempDir% del /s /q %tempDir% || goto :error
+if exist %tempDir% del /f /s /q %tempDir% || goto :error
 
 
 REM Generate Operations and Models for Debug purposes
-rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugOperations.cs.json || goto :error
-rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugModels.cs.json || goto :error
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugOperations.cs.json & exit
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json > debugModels.cs.json & exit
 java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l csharp -t Templates\csharp -o %tempDir% -c config.json || goto :error
 
 python Tools\split-cs-file.py %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\BarCodeApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
@@ -20,25 +20,24 @@ python Tools\split-cs-file.py %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\FileApi
 python Tools\split-cs-file.py %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\FolderApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
 python Tools\split-cs-file.py %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\StorageApi.cs %tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model\Requests\ || goto :error
 
+copy /y Templates\LICENSE "%targetDir%\" > NUL || goto :error
+copy /y Templates\LICENSE "%targetDir%\src\LICENSE.txt" > NUL || goto :error
 
-del /s /q "%targetDir%\src\Model\" > NUL || goto :error
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model" "%targetDir%\src\Model\" /e /i /y > NUL || goto :error
+rmdir /s /q "%targetDir%\src\Model\" > NUL || goto :error
+move /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Model" "%targetDir%\src" > NUL || goto :error
 
-del /s /q "%targetDir%\src\Api\*Api.cs" > NUL || goto :error
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\*.cs" "%targetDir%\src\Api\" /e /i /y > NUL || goto :error
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Client\Configuration.cs" "%targetDir%\src\Api\" /y > NUL || goto :error
+del /f /s /q "%targetDir%\src\Api\*Api.cs" > NUL || goto :error
+move /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Api\*.cs" "%targetDir%\src\Api\" > NUL || goto :error
+move /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Client\Configuration.cs" "%targetDir%\src\Api\" > NUL || goto :error
 
-del /s /q "%targetDir%\docs" > NUL || goto :error
-xcopy "%tempDir%\docs" "%targetDir%\docs" /e /i /y > NUL || goto :error
+rmdir /s /q "%targetDir%\docs" > NUL || goto :error
+move /y "%tempDir%\docs" "%targetDir%\" > NUL || goto :error
 
-xcopy "%tempDir%\README.md" "%targetDir%\" /y > NUL || goto :error
-xcopy "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Aspose.BarCode.Cloud.Sdk.csproj" "%targetDir%\src\Aspose.BarCode.Cloud.Sdk.csproj" /y > NUL || goto :error
+move /y "%tempDir%\README.md" "%targetDir%\README.template" > NUL || goto :error
+move /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk\Aspose.BarCode.Cloud.Sdk.csproj" "%targetDir%\src\Aspose.BarCode.Cloud.Sdk.csproj" > NUL || goto :error
+copy /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk.Test\Aspose.BarCode.Cloud.Sdk.Test.csproj" "%targetDir%\examples\GenerateQR\GenerateQR.csproj" > NUL || goto :error
+copy /y "%tempDir%\src\Aspose.BarCode.Cloud.Sdk.Test\Aspose.BarCode.Cloud.Sdk.Test.csproj" "%targetDir%\examples\ReadQR\ReadQR.csproj" > NUL || goto :error
 
-copy Templates\LICENSE "%targetDir%\" /y > NUL || goto :error
-copy Templates\LICENSE "%targetDir%\src\LICENSE.txt" /y > NUL || goto :error
 
-
-del /s /q %tempDir% > NUL || goto :error
-rmdir /s /q %tempDir% > NUL || goto :error
-
-pushd "%targetDir%" && .\scripts\format.cmd & popd
+rem rmdir /s /q %tempDir% > NUL || goto :error
+pushd "%targetDir%" && wsl make after-gen & popd
