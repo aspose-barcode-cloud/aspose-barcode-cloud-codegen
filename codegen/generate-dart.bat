213f75a3b6c4b758d76de7bd9b1200d19798e036
Denis Averin <denis.averin@aspose.com>
2022-03-31 23:44:43 +0700

    Dart release 22.3
    Dart submodule added

--- codegen/generate-dart.bat
+++ codegen/generate-dart.bat
@@ -5,40 +5,45 @@ set specSource=..\spec\aspose-barcode-cloud.json
 set tempDir=.generated\dart
 
 set targetDir=..\submodules\dart
-set targetDir=..\..\flutter\aspose-barcode-cloud-dart
 
 if exist %tempDir% del /s /q %tempDir% || goto :error
 
-rem java -jar Tools\swagger-codegen-cli.jar config-help -l dart
+rem java -jar Tools\swagger-codegen-cli.jar config-help -l dart & exit
+rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l dart -t Templates\dart -o %tempDir% -c config-dart.json > debugModels.dart.json & exit
+rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l dart -t Templates\dart -o %tempDir% -c config-dart.json > debugOperations.dart.json & exit
 java -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l dart -t Templates\dart -o %tempDir% -c config-dart.json || goto :error
-rem java -DdebugModels -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l dart -t Templates\dart -o %tempDir% -c config-dart.json > debugModels.dart.json || goto :error
-rem java -DdebugOperations -jar Tools\swagger-codegen-cli.jar generate -i "%specSource%" -l dart -t Templates\dart -o %tempDir% -c config-dart.json > debugOperations.dart.json || goto :error
 
 
-mkdir "%targetDir%\barcode\" > NUL
-move /y "%tempDir%\pubspec.yaml" "%targetDir%\barcode\" > NUL || goto :error
+copy /y Templates\LICENSE "%targetDir%\" > NUL || goto :error
+move /y "%tempDir%\README.md" "%targetDir%" > NUL || goto :error
 move /y "%tempDir%\.gitignore" "%targetDir%\" > NUL || goto :error
+move /y "%tempDir%\pubspec.yaml" "%targetDir%\" > NUL || goto :error
 
-del /s /q "%targetDir%\barcode\lib\" > NUL
-rmdir /s /q  "%targetDir%\barcode\lib\" > NUL
 
-mkdir "%targetDir%\barcode\lib\" > NUL || goto :error
-move /y "%tempDir%\lib\*.dart" "%targetDir%\barcode\lib\" > NUL || goto :error
+del /s /q "%targetDir%\lib\" > NUL || mkdir "%targetDir%\lib\" > NUL || goto :error
 
-mkdir "%targetDir%\barcode\lib\model\" > NUL || goto :error
-move /y "%tempDir%\lib\model\*.dart" "%targetDir%\barcode\lib\model\" > NUL || goto :error
+move /y "%tempDir%\lib\*.dart" "%targetDir%\lib\" > NUL || goto :error
 
-mkdir "%targetDir%\barcode\lib\api\" > NUL || goto :error
-move /y "%tempDir%\lib\api\*.dart" "%targetDir%\barcode\lib\api\" > NUL || goto :error
+mkdir "%targetDir%\lib\model\" > NUL
+move /y "%tempDir%\lib\model\*.dart" "%targetDir%\lib\model\" > NUL || goto :error
+mkdir "%targetDir%\lib\api\" > NUL
+move /y "%tempDir%\lib\api\*.dart" "%targetDir%\lib\api\" > NUL || goto :error
 
-mkdir "%targetDir%\barcode\lib\auth\" > NUL || goto :error
-move /y "%tempDir%\lib\auth\authentication.dart" "%targetDir%\barcode\lib\auth\" > NUL || goto :error
-move /y "%tempDir%\lib\auth\oauth.dart" "%targetDir%\barcode\lib\auth\" > NUL || goto :error
+mkdir "%targetDir%\lib\auth\"
+move /y "%tempDir%\lib\auth\authentication.dart" "%targetDir%\lib\auth\" > NUL || goto :error
+move /y "%tempDir%\lib\auth\oauth.dart" "%targetDir%\lib\auth\" > NUL || goto :error
 
+
+del /s /q "%targetDir%\doc\" > NUL
+rmdir /s /q  "%targetDir%\doc\" > NUL
+mkdir "%targetDir%\doc\api\" > NUL || goto :error
+move /y "%tempDir%\docs\*Api.md" "%targetDir%\doc\api\" > NUL || goto :error
+mkdir "%targetDir%\doc\models\" > NUL || goto :error
+move /y "%tempDir%\docs\*.md" "%targetDir%\doc\models\" > NUL || goto :error
 
 rem Cleanup
 del /s /q %tempDir% > NUL || goto :error
 rmdir /s /q  %tempDir% > NUL || goto :error
 
 rem Format generated code etc.
-pushd "%targetDir%" && dart fix --apply && dart format . & popd
+pushd "%targetDir%" && wsl make after-gen & popd
