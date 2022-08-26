25814bbd6cc5dcce0b3d7e4eca40a1faa21dfb1d
ivankamkin <109517020+ivankamkin@users.noreply.github.com>
2022-08-26 11:36:45 +0500

    Update install-all-packages.bash

--- scripts/install-all-packages.bash
+++ scripts/install-all-packages.bash
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 set -u
-# sudo apt-get -y install wget
-# sudo apt-get install -y ca-certificates curl
-# #Add the Microsoft package signing key to list of trusted keys and add the package repository.
-# wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
-# rm packages-microsoft-prod.deb
+sudo apt-get -y install wget
+sudo apt-get install -y ca-certificates curl
+# Add the Microsoft package signing key to list of trusted keys and add the package repository.
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 # Add Dart repository
-# wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
-# echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
-#Add Kubernetes repositories
-# sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-# echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
 # #Update
-# sudo apt-get update
+sudo apt-get update
 #Install
 deb_packages_list="../doc/deb_packages.list"
 for package in $(cat ${deb_packages_list}) 
@@ -27,5 +24,3 @@ do
         sudo apt-get -y install $package
     fi
 done
-dotnet tool install -g dotnet-format
-composer global require friendsofphp/php-cs-fixer
