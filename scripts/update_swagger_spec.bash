ea2db7b8acd02c7d0575fab2c23bb507d13ed88a
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2023-01-30 12:22:21 +0500

    Update version, spec and android templates

--- scripts/update_swagger_spec.bash
+++ scripts/update_swagger_spec.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-curl https://api.aspose.cloud/v3.0/barcode/swagger/spec > spec/aspose-barcode-cloud.json
+curl https://barcode.qa.aspose.cloud/v3.0/barcode/swagger/spec > spec/aspose-barcode-cloud.json
