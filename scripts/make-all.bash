0f2088b720d66b39e6a26310416f92659ff12145
Denis Averin <denis.averin@aspose.com>
2023-02-01 18:00:00 +0700

    Fix warnings in Dart SDK

--- scripts/make-all.bash
+++ scripts/make-all.bash
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 pushd "../codegen"
-for d in $(find . -name "generate-*.bash"); do 
-    "$d" || echo "Error of executing $d"
+for d in generate-*.bash
+do
+    "./$d" || echo "Error of executing $d"
 done
 popd
