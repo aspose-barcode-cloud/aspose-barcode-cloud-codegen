5ca5e6e8422679d1270955d081352f843065b390
Denis Averin <denis.averin@aspose.com>
2023-11-19 22:47:25 +0700

    Add required packages and scripts

---
+++ doc/debian/scripts/install-dotnet.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+# https://learn.microsoft.com/en-us/dotnet/core/install/linux-debian
+
+wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt update
+sudo apt install dotnet-sdk-7.0
