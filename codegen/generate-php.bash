5c1206075fd08680d7c5e02ae289dc7808df0ed6
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2024-09-06 14:29:56 +0500

    Fixed PHP generation

--- codegen/generate-php.bash
+++ codegen/generate-php.bash
@@ -11,29 +11,29 @@ then
 fi
 
 # java -jar Tools/openapi-generator-cli.jar config-help -g php ; exit 1
-# java -DdebugModels -jar Tools/openapi-generator-cli.jar generate -i "${specSource}" -g php -t Templates/php -o "${tempDir}" -c config-php.json > debugModels.php.json
-# java -DdebugOperations -jar Tools/openapi-generator-cli.jar generate -i "${specSource}" -g php -t Templates/php -o "${tempDir}" -c config-php.json > debugOperations.php.json
+# java -DdebugModels -jar Tools/openapi-generator-cli.jar generate -i "${specSource}" -g php -t Templates/php -o "${tempDir}" -c config-php.json > debugModels.php.json; exit
+# java -DdebugOperations -jar Tools/openapi-generator-cli.jar generate -i "${specSource}" -g php -t Templates/php -o "${tempDir}" -c config-php.json > debugOperations.php.json; exit
 java -jar Tools/openapi-generator-cli.jar generate -i "${specSource}" -g php -t Templates/php -o "${tempDir}" -c config-php.json
 
-# python Tools/split-php-file.py "${tempDir}/SwaggerClient-php/lib/Api/GenerateApi.php" "${tempDir}/SwaggerClient-php/lib/Requests/"
-# python Tools/split-php-file.py "${tempDir}/SwaggerClient-php/lib/Api/ScanApi.php" "${tempDir}/SwaggerClient-php/lib/Requests/"
-# python Tools/split-php-file.py "${tempDir}/SwaggerClient-php/lib/Api/RecognizeApi.php" "${tempDir}/SwaggerClient-php/lib/Requests/"
-# python Tools/split-php-file.py "${tempDir}/SwaggerClient-php/lib/Api/StorageApi.php" "${tempDir}/SwaggerClient-php/lib/Requests/"
+python Tools/split-php-file.py "${tempDir}/lib/Api/GenerateApi.php" "${tempDir}/lib/Requests/"
+python Tools/split-php-file.py "${tempDir}/lib/Api/ScanApi.php" "${tempDir}/lib/Requests/"
+python Tools/split-php-file.py "${tempDir}/lib/Api/RecognizeApi.php" "${tempDir}/lib/Requests/"
+# python Tools/split-php-file.py "${tempDir}/lib/Api/StorageApi.php" "${tempDir}/lib/Requests/"
 
 rm -f "${targetDir}/src/Aspose/BarCode/"*Api.php
-mv "${tempDir}/SwaggerClient-php/lib/Api/"* "${targetDir}/src/Aspose/BarCode"
-mv "${tempDir}/SwaggerClient-php/lib/"*.php "${targetDir}/src/Aspose/BarCode"
+mv "${tempDir}/lib/Api/"* "${targetDir}/src/Aspose/BarCode"
+mv "${tempDir}/lib/"*.php "${targetDir}/src/Aspose/BarCode"
 
 rm -f "${targetDir}/src/Aspose/BarCode/Model/"*
-mv "${tempDir}/SwaggerClient-php/lib/Model/"* "${targetDir}/src/Aspose/BarCode/Model"
+mv "${tempDir}/lib/Model/"* "${targetDir}/src/Aspose/BarCode/Model"
 
 rm -f "${targetDir}/src/Aspose/BarCode/Requests/"*
-# mv "${tempDir}/SwaggerClient-php/lib/Requests/"* "${targetDir}/src/Aspose/BarCode/Requests/"
+mv "${tempDir}/lib/Requests/"* "${targetDir}/src/Aspose/BarCode/Requests/"
 
 rm -rf "${targetDir}/docs/"*
-mv "${tempDir}/SwaggerClient-php/docs/"* "${targetDir}/docs"
+mv "${tempDir}/docs/"* "${targetDir}/docs"
 
-mv "${tempDir}/SwaggerClient-php/README.md" "${targetDir}/README.template"
+mv "${tempDir}/README.md" "${targetDir}/README.template"
 cp ../LICENSE "${targetDir}/"
 cp ../scripts/check-badges.bash "$targetDir/scripts/"
 
