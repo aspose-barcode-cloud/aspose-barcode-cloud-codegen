0dafeaa891fd40ca932f637c8545dd9f4bc67330
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2024-09-16 17:02:21 +0500

    Generation of Go SDK fixed

--- codegen/generate-go.bash
+++ codegen/generate-go.bash
@@ -14,7 +14,7 @@ fi
 # java -jar Tools/openapi-generator-cli.jar config-help -l go
 java -jar Tools/openapi-generator-cli.jar generate -i "$specSource" -g go -t Templates/go -o $tempDir -c config-go.json
 # java -DdebugModels -jar Tools/openapi-generator-cli.jar generate -i "$specSource" -g go -t Templates/go -o $tempDir -c config-go.json > debugModels.go.json
-# java -DdebugOperations -jar Tools/openapi-generator-cli.jar generate -i "$specSource" -g go -t Templates/go -o $tempDir -c config-go.json > debugOperations.go.json
+# java -DdebugOperations -jar Tools/openapi-generator-cli.jar generate -i "$specSource" -g go -t Templates/go -o $tempDir -c config-go.json > debugOperations.go.json ; exit
 
 rm -rf "$targetDir/barcode"
 
