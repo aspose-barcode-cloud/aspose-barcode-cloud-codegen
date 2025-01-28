995d706dcea52f6de9405d92c55a308ab4533cce
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2025-01-28 11:01:32 +0500

    Latest changes for templates and configs

--- scripts/switch_all_submodules_to_main.bash
+++ scripts/switch_all_submodules_to_main.bash
@@ -7,7 +7,7 @@ pushd "$(dirname "$0")/../submodules"
 for d in */ ; do
     pushd "$d"
 
-    (git fetch --prune && git switch main && git pull --ff-only) || true
+    (git fetch --prune && git switch v4 && git pull --ff-only) || true
 
     popd >/dev/null
 done
