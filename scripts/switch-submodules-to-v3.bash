d5376b33a4e525fc88460b6ea2c2e31c82d2b0e1
Denis Averin <denis.averin@aspose.com>
2025-05-31 18:54:05 +0700

    Update submodules

--- scripts/switch-submodules-to-v3.bash
+++ scripts/switch-submodules-to-v3.bash
@@ -8,8 +8,11 @@ for d in */ ; do
     pushd "$d"
 
     branch="v3"
-    if [ "$d" = "go/" ]; then
+    if [ "$d" = "dart/" ] || [ "$d" = "go/" ]; then
         branch="v1"
+    elif [ "$d" = "android/" ]; then
+        # Do not update Android to legacy v3 versions
+        branch="main"
     fi
     (git fetch --prune && git switch "$branch" && git pull --ff-only) || true
 
