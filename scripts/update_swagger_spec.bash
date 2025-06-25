4a3d3716f4bd5cb86eecdf2045952ef961e43c05
ivankamkin <109517020+ivankamkin@users.noreply.github.com>
2025-06-25 11:18:55 +0500

    Release 25.6 (#125)
    * Update version and templates
    
    * Fix check-urls.py
    
    ---------
    
    Co-authored-by: Denis Averin <denis.averin@aspose.com>

--- scripts/update_swagger_spec.bash
+++ scripts/update_swagger_spec.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# SWAGGER_SPEC_URL="https://api.aspose.cloud/v3.0/barcode/swagger/spec"
+# SWAGGER_SPEC_URL="https://barcode.qa.aspose.cloud/v4.0/barcode/swagger/spec"
 SWAGGER_SPEC_URL="https://api.aspose.cloud/v4.0/barcode/swagger/spec"
-# SWAGGER_SPEC_URL="http://localhost:47972/v3.0/barcode/swagger/spec"
+# SWAGGER_SPEC_URL="http://localhost:47972/v4.0/barcode/swagger/spec"
 
 curl "${SWAGGER_SPEC_URL}" > spec/aspose-barcode-cloud.json
